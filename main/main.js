df2cfa60352335a7cc926241227c72f013fb6ea5
WuYiChenhml <1055200502@qq.com>
2017-07-14 17:14:32 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@
 module.exports = function main() {
     var sequence = [6, 9, 15, -2, 92, 11];
     var sum = 0;
-    var length = length;
+    var length = sequence.length;
     var max = Math.max.apply(Math, sequence);
     var min = Math.min.apply(Math, sequence);
 for(var i = 0 ; i < sequence.length ; i ++)
@@ -12,10 +12,7 @@ for(var i = 0 ; i < sequence.length ; i ++)
 
 var average = sum / length;
     
-    console.log('o) 最小值 = ' + min);
-    console.log('o) 最大值 = ' + max);
-    console.log('o) 元素数量 = ' + length);
-    console.log('o) 平均值 = ' + average);
+    console.log('o) 最小值 = ' + min +'/n' + 'o) 最大值 = ' + max + '/n' + 'o) 元素数量 = ' + length + '/n' + 'o) 平均值 = ' + average);
 };
     
 
