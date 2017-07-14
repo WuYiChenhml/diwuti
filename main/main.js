66b6375fe59bd91a0403fb180a62c98afd2e3f2d
WuYiChenhml <1055200502@qq.com>
2017-07-14 17:19:37 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -12,7 +12,10 @@ for(var i = 0 ; i < sequence.length ; i ++)
 
 var average = sum / length;
     average = average.toFixed(2);
-    console.log('o) 最小值 = ' + min +'/n' + 'o) 最大值 = ' + max + '/n' + 'o) 元素数量 = ' + length + '/n' + 'o) 平均值 = ' + average);
+    console.log('o) 最小值 = ' + min 
+                + 'o) 最大值 = ' + max 
+                + 'o) 元素数量 = ' + length 
+                + 'o) 平均值 = ' + average);
 };
     
 
